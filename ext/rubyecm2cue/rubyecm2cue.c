2989a93d2dda879886f57c8c890f72a94026b7c3
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-09-04 09:51:35 +0800

    update ignore file

--- ext/rubyecm2cue/rubyecm2cue.c
+++ ext/rubyecm2cue/rubyecm2cue.c
@@ -1,3 +1,19 @@
 #include "ruby.h"
 #include <stdio.h>
 #include "unecm.h"
+
+VALUE RubyEcm2Cue = Qnil;
+void Init_rubyecm2cue();
+VALUE method_process(VALUE, VALUE);
+
+void Init_rubyecm2cue(){
+    VALUE RubyEcm2Cue = rb_define_module("RubyEcm2Cue");
+    rb_define_method(RubyEcm2Cue, "process", method_process, 1);
+}
+
+VALUE method_process(VALUE self, VALUE arg_path) {
+    const char *img= StringValueCStr(arg_path);
+    const char *retValue;
+
+    return rb_str_new_cstr(retValue);
+}
