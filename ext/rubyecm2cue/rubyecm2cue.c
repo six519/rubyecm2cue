32d91ffa782bd630b9c5c0755d8c586394779f96
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-09-04 10:31:10 +0800

    apply ecm2cue

--- ext/rubyecm2cue/rubyecm2cue.c
+++ ext/rubyecm2cue/rubyecm2cue.c
@@ -12,8 +12,61 @@ void Init_rubyecm2cue(){
 }
 
 VALUE method_process(VALUE self, VALUE arg_path) {
-    const char *img= StringValueCStr(arg_path);
-    const char *retValue;
+    char *retValue = "";
+    char *infilename = StringValueCStr(arg_path);;
+    char *outfilename;
+    char *oldfilename;
+    int unecm_ret = 0;
+
+    FILE *fin, *fout, *cue_file;
+
+    eccedc_init();
+
+    if(strlen(infilename) < 5) {
+      strcpy(retValue, "filename is too short");
+    }
+    if(strcasecmp(infilename + strlen(infilename) - 4, ".ecm")) {
+      strcpy(retValue, "filename must end in .ecm");
+    }
+
+    outfilename = malloc(strlen(infilename) - 3);
+    oldfilename = malloc(strlen(infilename) - 3);
+    if(!outfilename) abort();
+    memcpy(outfilename, infilename, strlen(infilename) - 4);
+    outfilename[strlen(infilename) - 4] = 0;
+
+    fprintf(stderr, "Decoding %s to %s.\n", infilename, outfilename);
+
+    fin = fopen(infilename, "rb");
+    if(!fin) {
+      strcpy(retValue, "Cannot open file");
+    }
+    fout = fopen(outfilename, "wb");
+    if(!fout) {
+      strcpy(retValue, "Cannot write file");
+      fclose(fin);
+    }
+
+    unecm_ret = unecmify(fin, fout);
+
+    fclose(fout);
+    fclose(fin);
+
+    if(unecm_ret != 0) {
+      strcpy(retValue, "Cannot decode file");
+    }
+
+    strcpy(oldfilename, outfilename);
+    cue_file = fopen(strcat(outfilename, ".cue"), "w");
+    if(!cue_file) {
+      strcpy(retValue, "Cannot write cue file");
+    }
+    fprintf(cue_file, "FILE \"%s\" BINARY\n", oldfilename);
+    fprintf(cue_file, "\tTRACK 01 MODE1/2352\n");
+    fprintf(cue_file, "\t\tINDEX 01 00:00:00");
+    fclose(cue_file);
+
+    strcpy(retValue, outfilename);
 
     return rb_str_new_cstr(retValue);
 }
