f0f6fc57e6fbb549998b181178642dafec68f8f2
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-09-04 09:38:12 +0800

    first commit

---
+++ ext/rubyecm2cue/rubyecm2cue.c
@@ -0,0 +1,3 @@
+#include "ruby.h"
+#include <stdio.h>
+#include "unecm.h"
---
+++ ext/rubyecm2cue/unecm.h
@@ -0,0 +1,11 @@
+#ifndef UNECM_H
+#define UNECM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void eccedc_init(void);
+int unecmify(FILE *in,FILE *out);
+
+#endif
